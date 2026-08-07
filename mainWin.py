2e802c3789d4c9be8fff9df3919ec3eb2d33049d
arsenijstefanuk4-svg <arsebijstefanuk4@gmail.com>
2026-08-07 18:25:14 +0200

    1 вікно

--- mainWin.py
+++ mainWin.py
@@ -1,48 +1,46 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout,
-              QPushButton, QLabel, QLineEdit)
+QPushButton, QLabel, QLineEdit)
 
 from instr import *
-       
+
+
 class MainWin(QWidget):
-    def init(self):
-        super().init()
-        # створюємо та налаштовуємо графічні елементи:
+    def __init__(self):
+        super().__init__()
+    # створюємо та налаштовуємо графічні елементи:
         self.initUI()
-        #Встановлює зв'язки між елементами
+    # Встановлює зв'язки між елементами
         self.connects()
-        #Встановлює, як виглядатиме вікно (напис, розмір, місце)
+    # Встановлює, як виглядатиме вікно (напис, розмір, місце)
         self.set_appear()
-        # старт:
+    # старт:
         self.show()
+
     def initUI(self):
-        ''' створює графічні елементи '''
+
         self.btn_next = QPushButton(txt_next, self)
         self.hello_text = QLabel(txt_hello)
+
         self.layout_line = QVBoxLayout()
-        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.hello_text, alignment=Qt.AlignCenter)
+        self.layout_line.addWidget(self.btn_next, alignment=Qt.AlignCenter)
         self.setLayout(self.layout_line)
 
     def connects(self):
         self.btn_next.clicked.connect(self.next_click)
+
     def next_click(self):
         self.hide()
-        self.tw = TestWin()
 
-    ''' встановлює, як виглядатиме вікно (напис, розмір, місце) '''
     def set_appear(self):
+
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
 
-
-
-
-app = QApplication([])
-mw = MainWin()
-main_win = QWidget()
-main_win.show()
-app.exec_()
-
-        
-
+if __name__ == '__main__':
+ app = QApplication([])
+ mw = MainWin()
+ mw.show()
+ app.exec_()
