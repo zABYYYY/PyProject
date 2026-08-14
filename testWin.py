c8e8a9f4e15474e0da12fee9b09248863e46e95a
arsenijstefanuk4-svg <arsebijstefanuk4@gmail.com>
2026-08-14 18:00:28 +0200

    ///

---
+++ testWin.py
@@ -0,0 +1,90 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout,
+QPushButton, QLabel, QLineEdit)
+
+from mainWin import MainWin
+
+testWin.setWindowTitle("Здоров'я")
+
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+
+class testWin(QWidget):
+    def __init__(self):
+        super().__init__()
+    # створюємо та налаштовуємо графічні елементи:
+        self.initUI()
+    # Встановлює зв'язки між елементами
+        self.connects()
+    # Встановлює, як виглядатиме вікно (напис, розмір, місце)
+        self.set_appear()
+    # старт:
+        self.show()
+
+    def initUI(self):
+        
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment=Qt.AlignCenter)
+        self.layout_line.addWidget(self.btn_next, alignment=Qt.AlignCenter)
+        self.setLayout(self.layout_line)
+
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+
+    def next_click(self):
+        self.hide()
+
+
+    def set_appear(self):
+
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app = QApplication([])
+main_win = MainWin()
+main_win.show()
+app.exec_()
